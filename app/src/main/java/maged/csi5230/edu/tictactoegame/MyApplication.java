c45162df8d12d073a01d8dbe92eab9719a23d26b
dragonlayout <dragonlayoutt@gmail.com>
2017-11-12 22:45:56 +0800

    moving part of the game, need more test

---
+++ app/src/main/java/maged/csi5230/edu/tictactoegame/MyApplication.java
@@ -0,0 +1,10 @@
+package maged.csi5230.edu.tictactoegame;
+
+import android.app.Application;
+
+/**
+ * Created by dragonlayout on 2017/11/12.
+ */
+
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/maged/csi5230/edu/tictactoegame/utils/Constants.java
@@ -0,0 +1,8 @@
+package maged.csi5230.edu.tictactoegame.utils;
+
+/**
+ * Created by dragonlayout on 2017/11/11.
+ */
+
+public class Constants {
+}
